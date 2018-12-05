cfe7fc58e0c1efd91e0c45677ced721bd1b2581d
Marczu <marczu09@gmail.com>
2018-12-05 18:38:58 +0100

    Add styles and fetching data from json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react'
-import {robots} from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox"
+import './App.css';
 
 class App extends Component {
 
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
     }
 
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => {
+                this.setState({robots: users})
+                console.log(users)
+            });
+    }
+
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
-    }
+    };
 
     render() {
 
@@ -25,7 +35,7 @@ class App extends Component {
 
         return (
             <div className='tc'>
-                <h1>Robotos Consulting</h1>
+                <h1 className='f1'>Robotos Consulting</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
